73069ab9d18377ebf54de140083de17f948bb0cc
Jirawat Boonsumret <58130500107@st.sit.kmutt.ac.th>
2016-10-11 08:34:52 +0700

    add draf model

--- src/java/int303/project/etc/TestConnectionBuilderMySql.java
+++ src/java/int303/project/etc/TestConnectionBuilderMySql.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package int303.project.model;
+package int303.project.etc;
 
+import int303.project.model.ConnectionBuilderMySql;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
---
+++ src/java/int303/project/model/AddStaff.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package int303.project.model;
+
+/**
+ *
+ * @author jiraw
+ */
+public class AddStaff {
+
+    private int staffId;
+    private int companyId;
+    private String firstname;
+    private String lastname;
+    private long citizenNo;
+    private String address;
+    private String position;
+
+    public int getStaffId() {
+        return staffId;
+    }
+
+    public void setStaffId(int staffId) {
+        this.staffId = staffId;
+    }
+
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(int companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public long getCitizenNo() {
+        return citizenNo;
+    }
+
+    public void setCitizenNo(long citizenNo) {
+        this.citizenNo = citizenNo;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+    
+    
+}
